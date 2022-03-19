cdaafcaa5af4d16b75fc2342ce605f101d2fa3b4
Alessandro Candido <candido.ale@gmail.com>
2022-03-19 19:17:58 +0100

    Reorganize project

--- packages/create-show/src/index.ts
+++ packages/create-show/src/index.ts
@@ -3,12 +3,13 @@
 import { program } from "commander";
 import { new_ } from "@annibale/show-cli";
 import process from "process";
+import { create as createSvelte } from "create-svelte";
 
 console.log(process.argv);
 process.argv.splice(2, 0, "new");
 console.log(process.argv);
 
-async function run() {
+async function main() {
   program.addCommand(new_());
   program.version(process.env.npm_package_version!);
 
